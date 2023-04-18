a2f9a3449ba93659ac35e6b09c3a3fea6c4bdafc
FoxSylv <atheriault@bowdoin.edu>
2023-04-18 19:59:48 -0400

    Add proper crediting.

--- content-scripts/vanityChanges.js
+++ content-scripts/vanityChanges.js
@@ -12,5 +12,19 @@ browser.storage.local.get("isEnabled", (values) => {
         caption.innerHTML = `DON'T YOU LECTURE ME WITH YOUR THIRTY DOLLAR <img src=${browser.runtime.getURL("data/images/noteblock_harp.png")} alt="Noteblock image" style="height: 50px; margin-left: 6.9px; transform: translateY(25%);"></img>`;
         caption.removeAttribute("id");
         caption.style = "cursor: auto;";
+
+        //Extension credits
+        const credits = document.getElementsByClassName("credits")[0];
+        credits.innerHTML = `<p>Original Website by <a target="_blank" href="https://twitter.com/TheRealGDColon">Colon</a>:</p>
+                             <p>Extension by <a target="_blank" href="https://github.com/FoxSylv">FoxSylv</a> ^w^</p>`;
+        credits.removeAttribute("style");
+        var plznokill = document.createElement("div");
+        plznokill.innerHTML = "<p>All sounds owned by Mojang please don't kill me >w<</p>";
+        plznokill.setAttribute("class", "credits");
+        plznokill.style = "margin-top: 0;";
+        credits.after(plznokill);
+
+        //Remove reference to "my" silly projects (to remove ambiguity)
+        credits.previousElementSibling.remove();
     } //end if(values.isEnabled)
 }); //end browser.storage.local.get()
